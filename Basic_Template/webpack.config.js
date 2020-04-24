94c0a6dfad77ab84bdecbd5984f9ec1378366a45
knoepfda <daniel.knoepfel@live.de>
2020-04-24 20:40:39 +0200

    minifying css

--- Basic_Template/webpack.config.js
+++ Basic_Template/webpack.config.js
@@ -1,16 +1,17 @@
-var webpack = require('webpack');
-var path = require('path');
-var package = require('./package.json');
+const webpack = require('webpack');
+const path = require('path');
+const package = require('./package.json');
 
 // variables
-var isProduction = process.argv.indexOf('-p') >= 0 || process.env.NODE_ENV === 'production';
-var sourcePath = path.join(__dirname, './src');
-var outPath = path.join(__dirname, './build');
+const isProduction = process.argv.indexOf('-p') >= 0 || process.env.NODE_ENV === 'production';
+const sourcePath = path.join(__dirname, './src');
+const outPath = path.join(__dirname, './build');
 
 // plugins
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'); // see https://web.dev/minify-css/
 
 module.exports = {
     context: sourcePath,
@@ -131,6 +132,15 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[hash].css',
             disable: !isProduction,
+        }),
+        new OptimizeCSSAssetsPlugin({
+            // // this would work too but no sourcemap would be generated:  new OptimizeCSSAssetsPlugin({}),
+            // see  https://github.com/NMFR/optimize-css-assets-webpack-plugin/issues/53
+            cssProcessorOptions: {
+                map: {
+                    inline: false,
+                },
+            },
         }),
         new HtmlWebpackPlugin({
             template: 'assets/index.html',
