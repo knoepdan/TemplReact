a57f2e1f7d126c5aaeaeb5916e063b8e6ace916c
knoepfda <daniel.knoepfel@live.de>
2020-04-27 23:08:15 +0200

    All Templates: improved typeings

--- Basic_Template/types/global.d.ts
+++ Basic_Template/types/global.d.ts
@@ -1,4 +1,13 @@
-/** Global definitions for development **/
+/// <reference types="node" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+        NODE_ENV: 'development' | 'production' | 'test';
+        //PUBLIC_URL: string; -> not needed
+    }
+}
 
 // for style loader
 declare module '*.css' {
@@ -34,3 +43,24 @@ declare module '*.gif' {
 // Omit type https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-377567046
 //type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 type PartialPick<T, K extends keyof T> = Partial<T> & Pick<T, K>;
+
+/* other ways of definitions devinitions (used by react-scripts) */
+/*
+declare module '*.png' {
+    const src: string;
+    export default src;
+  }
+  declare module '*.svg' {
+    import * as React from 'react';
+  
+    export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  
+    const src: string;
+    export default src;
+  }
+  
+  declare module '*.module.css' {
+    const classes: { [key: string]: string };
+    export default classes;
+  }
+  */
